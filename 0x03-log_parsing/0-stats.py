ccdf2025f9d7238bdcc97c8f0ab6ffce7509b5d3
Valeryio <tommagoma23@gmail.com>
2024-11-28 09:48:17 +0000

    Upddate the task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""This methods helps to analyse logs
+and parse them
+"""
+
+import sys
+
+result = sys.stdin.read()
+
+print("Read from stdin : ", result)
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,7 @@
+# Log Parsing
+
+## General
+- Learn how to parse logs with python script
+
+## Student
+Linson DMT
