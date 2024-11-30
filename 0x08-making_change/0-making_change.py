f3798a61ba4505b42dcd549b4a28d7330907fae0
Valeryio <tommagoma23@gmail.com>
2024-11-30 10:16:03 +0000

    commit the task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+
+"""Module of an algorithm that is non-greedy
+"""
+
+
+def makeChange(coins, total):
+    """This function makes some changes
+    to determine one of the best way to find
+    coins and sum them up ti tital
+
+    param: coins
+    param: total
+    """
+
+    if total <= 0 or len(coins) == 0:
+        return 0
+
+    # sort the array by reverse order
+    coins.sort(reverse=True)
+    pieces, i = 0, 0
+
+    while i < len(coins) and total != 0:
+        if total < coins[i]:
+            i += 1
+            continue
+        else:
+            total -= coins[i]
+            pieces += 1
+            if total >= coins[i]:
+                pass
+            else:
+                i += 1
+
+    if total == 0:
+        return pieces
+    else:
+        return -1
