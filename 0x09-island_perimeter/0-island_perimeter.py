3f0bfb6b1637b854f9d6fe75222da507dc6316cc
Valeryio <tommagoma23@gmail.com>
2024-12-03 11:31:54 +0000

    commit the task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,23 +14,15 @@ def island_perimeter(grid):
         A number
     """
     perimeter = 0
-    # print(len(grid), " : " ,len(grid[0]))
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            # print(f"[{grid[i][j]}]", end='')
-
             if grid[i][j] == 1:
-                # verify at the top
                 if grid[i - 1][j] == 0:
                     perimeter += 1
                 if grid[i-1][j] == 0:
-                # verify at the bottom
                     perimeter += 1
                 if grid[i][j - 1] == 0:
-                # verify at the left
                     perimeter += 1
                 if grid[i][j + 1] == 0:
-                # verify at the right
                     perimeter += 1
-        # print('')
     return perimeter
